478fafd7b3be59ec7e031cade8009435be846cef
federicosimone <federico.simone15@gmail.com>
2025-12-29 11:13:27 +0100

    navbar ok

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,14 @@
 import logo from '../assets/logo.png'
+import style from '../components/Header.module.css'
+
+const navbarList = [
+    { id: 1, title: "Home", link: "#" },
+    { id: 2, title: "I punti di forza", link: "#" },
+    { id: 3, title: "Recensioni", link: "#" },
+    { id: 4, title: "Paperelle", link: "#" },
+    { id: 5, title: "FAQs", link: "#" },
+    { id: 6, title: "Newsletter", link: "#" }
+];
 
 
 export default function Header() {
@@ -6,18 +16,40 @@ export default function Header() {
         <>
             <nav className="navbar bg-body-tertiary">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">
-                        <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
-                    </a>
-                    <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i className="fa-solid fa-bars fs-2"></i></button>
+                    {/*LOGO*/}
+                    <div>
+                        <a className="navbar-brand" href="#">
+                            <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
+                        </a>
+                    </div>
+                    {/*LISTA NAVBAR*/}
+                    <ul className="list-unstyled d-md-flex gap-5 mb-0 d-none me-4">
 
-                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
-                        <div className="offcanvas-header">
-                            <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
-                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                        </div>
-                        <div class="offcanvas-body">
-                            ...
+                        {
+                            navbarList.map((item) => {
+                                return <li key={item.id}><a className="text-decoration-none" href={item.link}>{item.title}</a></li>
+                            })
+                        }
+                    </ul>
+                    {/*menu a tendina, presente solo nel mobile*/}
+                    <div className="d-md-none d-lg-none">
+                        <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i className="fa-solid fa-bars fs-2"></i></button>
+
+                        <div className="offcanvas offcanvas-end w-75" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                            <div className="offcanvas-header">
+                                <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
+                                <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                            </div>
+                            <div class="offcanvas-body">
+                                <ul className="list-unstyled">
+
+                                    {
+                                        navbarList.map((item) => {
+                                            return <li className="pb-3" key={item.id}><a className="text-decoration-none" href={item.link}>{item.title}</a></li>
+                                        })
+                                    }
+                                </ul>
+                            </div>
                         </div>
                     </div>
                 </div>
