fbd6d8aee49c2cbee6aabeda95e7da5b547a1036
federicosimone <federico.simone15@gmail.com>
2025-12-29 10:09:14 +0100

    impostazione navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,32 @@
+import logo from '../assets/logo.png'
 
 
 export default function Header() {
     return (
         <>
-            <div>Ciao</div>
+            <nav className="navbar bg-body-tertiary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        <img src={logo} alt="Logo" width="50" height="50" className="d-inline-block align-text-top" />
+                    </a>
+                    <button className="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight"><i className="fa-solid fa-bars fs-2"></i></button>
+
+                    <div className="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+                        <div className="offcanvas-header">
+                            <h5 className="offcanvas-title" id="offcanvasRightLabel">Offcanvas right</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            ...
+                        </div>
+                    </div>
+                </div>
+            </nav>
         </>
     );
 }
+
+
 
 
 
